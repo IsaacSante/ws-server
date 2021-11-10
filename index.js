9100297214b5f06dda28843d6de4b238a6b9a8b4
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-11-10 00:22:59 -0500

    convert data to string

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT });
 
-console.log('server is running')
+console.log("server is running");
 
-wss.on("connection", ws => {
-    console.log("New Client Connected");
-    ws.send('im the Heroku ws server');
+wss.on("connection", (ws) => {
+  console.log("New Client Connected");
+  ws.send("im the Heroku ws server");
 
-  ws.on("message", data => {
-    console.log(`Client has sent + ${data}`)
+  ws.on("message", (data) => {
+    data = data.toString();
+    console.log(`Client has sent + ${data}`);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
-  })
+  });
 
   ws.on("close", () => {
-    console.log("Client has disconnected")
-  })
-})
+    console.log("Client has disconnected");
+  });
+});
