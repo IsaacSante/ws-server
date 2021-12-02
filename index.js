4e7e3dd56afe69986bb0d0cc820601d6643735d1
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-12-01 19:04:24 -0500

    now parsing json data

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ wss.on("connection", (ws) => {
   ws.send("im the Heroku ws server");
 
   ws.on("message", (data) => {
-    data = data.toString();
     console.log(`Client has sent + ${data}`);
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
+    if (isJSON(data)) {
+      let currData = JSON.parse(data);
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(currData);
+        }
+      });
+    } else if (typeof data === "string") {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    } else {
+      console.log("data unknown");
+    }
   });
 
   ws.on("close", () => {
